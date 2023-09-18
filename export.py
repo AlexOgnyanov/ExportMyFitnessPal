f10ba29cf3ffe27649ca7389e4fa18098c3dd82a
AlexOgnyanov <133657810+AlexOgnyanov@users.noreply.github.com>
2023-09-18 14:16:43 +0300

    Multiple browser support

--- export.py
+++ export.py
@@ -1,31 +1,68 @@
 import myfitnesspal, pathlib
 from write import *
 from datetime import datetime
+import json
+import pygsheets
+import browser_cookie3
+
+def load_config():
+  f = open("config.json", "r")
+  config = json.load(f)
+  f.close()
+  return config
+
+config = load_config()
 
 pathToCredentials = pathlib.Path("./credentials.json").absolute()
 gc = pygsheets.authorize(service_file=pathToCredentials)
 
-sh = gc.open("Alex Ognyanov - PWL")
+# Find the spreadsheet with the name defined in the config
+sh = gc.open(config["SpreadsheetName"])
 
-sheet_index = get_sheet_index("ДИЕТА", sh)
+# Find the index of the sheet with the name defined in the config
+sheet_index = get_sheet_index(config["WorksheetName"], sh)
 if(sheet_index == -1):
     print("Sheet not found.")
     raise SystemExit
 
 working_sheet = sh[sheet_index]
 
-client = myfitnesspal.Client()
-while(get_last_written_date_sheet(working_sheet) != -1):
+cookie_jar = None
+
+if config["Browser"] == "Chrome":
+  cookie_jar = browser_cookie3.chrome(domain_name="myfitnesspal.com")
+elif config["Browser"] == "Firefox":
+  cookie_jar = browser_cookie3.firefox(domain_name="myfitnesspal.com")
+elif config["Browser"] == "Edge":
+  cookie_jar = browser_cookie3.edge(domain_name="myfitnesspal.com")
+elif config["Browser"] == "Opera":
+  cookie_jar = browser_cookie3.opera(domain_name="myfitnesspal.com")
+elif config["Browser"] == "Safari":
+  cookie_jar = browser_cookie3.safari(domain_name="myfitnesspal.com")
+else: 
+  print("Browser not supported.")
+  raise SystemExit
+
+client = myfitnesspal.Client(cookiejar=cookie_jar)
+date_to_get = get_last_written_date_sheet(working_sheet).split(".")
+while(date_to_get != -1):
+  # Split date string into day, month and year
   date_to_get = get_last_written_date_sheet(working_sheet).split(".")
+  print("Getting data for ", date_to_get, "...")
+  #check if date is in the future
   if datetime(int(date_to_get[2]), int(date_to_get[1]), int(date_to_get[0])) > datetime.now():
     print("All dates are written.")
     raise SystemExit
-
-  print(date_to_get)
+  
   date = client.get_date(date_to_get[2], date_to_get[1], date_to_get[0])
-  print(date.totals)
 
-  if not date.totals:   
-    write_data_to_sheet(["-", "-", "-", "-"], working_sheet)
+  # If no data is found for the date, write - - - - to the sheet
+  if not date.totals:
+    write_data_to_sheet(["-", "-", "-", "-", config["1"]], working_sheet)
+    print(f"Written - - - - {config['1']} for {date_to_get[0]}.{date_to_get[1]}.{date_to_get[2]}")
   else:
-    write_data_to_sheet([date.totals['calories'], date.totals['protein'], date.totals['carbohydrates'], date.totals['fat']], working_sheet)
+    write_data_to_sheet([date.totals['calories'], date.totals['protein'], date.totals['carbohydrates'], date.totals['fat'], config["5"]], working_sheet)
+  
+    print(f"Written {[date.totals['calories'], date.totals['protein'], date.totals['carbohydrates'], date.totals['fat'], {config['5']}]} for {date_to_get[0]}.{date_to_get[1]}.{date_to_get[2]}")
+
+print("All data written to sheet.")
--- write.py
+++ write.py
@@ -1,4 +1,3 @@
-import pygsheets
 from dateutil.parser import parse
 
 def is_date(string, fuzzy=False):
@@ -16,7 +15,12 @@ def is_date(string, fuzzy=False):
         return False
 
 def get_sheet_index(sheetName, sh):
-    #select the first sheet 
+    """
+    Returns the index of the sheet with the desired sheetName.
+
+    :param sheetName: str, name of the sheet to find
+    :param sh: pygsheets.Spreadsheet, spreadsheet to search in
+    """
     for index, i in enumerate(sh):
         if i.title == sheetName:
             return index
@@ -24,21 +28,47 @@ def get_sheet_index(sheetName, sh):
     return -1
 
 def get_last_written_row(sheet_data):
+    """
+    Gets the index of the last written row in the sheet.
+    (A column should be a valid date and F column should be empty to be considered as a valid row)
+    """
     for index, i in enumerate(sheet_data):
         if is_date(i[0]) and i[5] == "":
             return index
         
     return -1
 
+def get_last_written_row_sheet(working_sheet):
+    """
+    Same as get_last_written_row, but takes a pygsheets.Worksheet as an argument.
+    """
+    sheet_data = working_sheet.get_all_values()
+    return get_last_written_row(sheet_data)
+
 def get_last_written_date(sheet_data):
+    """
+    Returns the date of the last written row in the sheet. (Column A)
+    """
     return sheet_data[get_last_written_row(sheet_data)][0]
 
 def get_last_written_date_sheet(working_sheet):
+    """
+    Same as get_last_written_date, but takes a pygsheets.Worksheet as an argument.
+    """
     sheet_data = working_sheet.get_all_values()
-    return sheet_data[get_last_written_row(sheet_data)][0]
+    return get_last_written_date(sheet_data)
 
 def write_data_to_sheet(data, working_sheet):
-    if(len(data) != 4):
+    """
+    Writes the data to the sheet.
+    Data should be in the format [calories, protein, carbohydrates, fat, comment]
+
+    (Works with columns F:J)
+    
+    :param data: list, data to write to the sheet
+    :param working_sheet: pygsheets.Worksheet, sheet to write to
+    """
+    if(len(data) != 5):
         print("Data not in correct format.")
         raise SystemExit
     
@@ -46,7 +76,7 @@ def write_data_to_sheet(data, working_sheet):
 
     last_written_row = get_last_written_row(sheet_data)
     if(last_written_row == -1):
-        print("Last writter row not found.")
+        print("Last written row not found.")
         raise SystemExit
 
-    working_sheet.update_values(crange=f'F{last_written_row+1}:I{last_written_row+1}', values=[data])
+    working_sheet.update_values(crange=f'F{last_written_row+1}:J{last_written_row+1}', values=[data])
